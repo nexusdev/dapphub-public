0b2b465cd15da28f02ba4436aa75329862a3ad28
cass <cass@bitshares.org>
2016-03-06 13:06:26 +0100

    clean ups

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
 $(document).foundation();
 
-/* -----------------------------------------------
- /* How to use? : Check the GitHub README
- /* ----------------------------------------------- */
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
- particlesJS.load('particles-js', 'particles.json', function() {
- console.log('particles.js loaded - callback');
- });
- */
-
-/* Otherwise just put the config content (json): */
+/* particlesJS */
 
 particlesJS('particles-js',
 
